372fd55626dad1ea2063d6fc3a9bc94bf905db43
Владіслав Одуха <Odukha_vladislav@ukr.net>
2021-05-26 10:25:49 +0300

    Доробив форму, написав гавнокод

--- ProjectTechMech/Form1.Designer.cs
+++ ProjectTechMech/Form1.Designer.cs
@@ -29,12 +29,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(415, 108);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(167, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(415, 139);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(167, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(182, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(177, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Вхідні данні (варіант №4):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(163, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(246, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Частота обертання на виході nв, хв.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(163, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Потужність на виході Pв, кВт";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(616, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 50);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Розрахунок";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(415, 198);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
--- ProjectTechMech/Form1.cs
+++ ProjectTechMech/Form1.cs
@@ -16,5 +16,91 @@ namespace ProjectTechMech
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Pv = double.Parse(textBox_P.Text);
+            double nv = double.Parse(textBox_n.Text);
+
+            textBox3.Text = Pv.ToString();
+            textBox3.Text = nv.ToString();
+
+
+            double nZag = 0.903;
+            double Ppotr = Pv / nZag;
+
+            double nF = 950;
+
+
+            double Uzag = nF / nv;
+            double Up = Uzag / 3;
+
+            double UpGOST = 1.0;
+
+            if (Up > 1)
+            { UpGOST = 1.12; }
+            else if (Up > 1.12)
+            { UpGOST = 1.25; }
+            else if (Up > 1.25)
+            { UpGOST = 1.4; }
+            else if (Up > 1.4)
+            { UpGOST = 1.6; }
+            else if (Up > 1.6)
+            { UpGOST = 1.8; }
+            else if (Up > 1.8)
+            { UpGOST = 2.0; }
+            else if (Up > 2.0)
+            { UpGOST = 2.24; }
+            else if (Up > 2.24)
+            { UpGOST = 2.5; }
+            else if (Up > 2.5)
+            { UpGOST = 2.8; }
+            else if (Up > 2.8)
+            { UpGOST = 3.15; }
+            else if (Up > 3.15)
+            { UpGOST = 3.55; }
+            else if (Up > 3.55)
+            { UpGOST = 4.00; }
+            else if (Up > 4.00)
+            { UpGOST = 4.5; }
+            else if (Up > 4.5)
+            { UpGOST = 5.0; }
+            else if (Up > 5.0)
+            { UpGOST = 5.6; }
+            else if (Up > 5.6)
+            { UpGOST = 6.3; }
+            else if (Up > 6.3)
+            { UpGOST = 7.1; }
+            else if (Up > 7.1)
+            { UpGOST = 8.0; }
+            else if (Up > 8.0)
+            { UpGOST = 9.0; }
+            else if (Up > 9.0)
+            { UpGOST = 10.0; }
+            else if (Up > 10.0)
+            { UpGOST = 11.2; }
+            else if (Up > 11.2)
+            { UpGOST = 12.5; }
+
+
+
+            double Upp = Uzag / UpGOST;
+
+            textBox3.Text = Upp.ToString();
+
+            double n1 = nF;
+            double n2 = n1 / Upp;
+            double n3 = n2 / UpGOST;
+
+            //похибка ага
+
+            double w1 = (Math.PI * n1) / 30;
+            double w2 = (Math.PI * n2) / 30;
+            double w3 = (Math.PI * n3) / 30;
+
+            double T1 = (Ppotr * 1000) / w1;
+            double T2 = (Ppotr * 0.95 * 0.99 * 1000) / w2;
+            double T3 = (Pv * 1000) / w3;
+        }
     }
 }
