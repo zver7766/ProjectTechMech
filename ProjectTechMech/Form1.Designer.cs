48c8477c179abbf689ba807ae454db204688382d
Владіслав Одуха <Odukha_vladislav@ukr.net>
2021-05-30 11:25:31 +0300

    Всьо зробив, окрім
    21
    156
    173
    
     строчки

--- ProjectTechMech/Form1.Designer.cs
+++ ProjectTechMech/Form1.Designer.cs
@@ -29,92 +29,104 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBox_P = new System.Windows.Forms.TextBox();
             this.textBox_n = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox_P
             // 
-            this.textBox_P.Location = new System.Drawing.Point(415, 108);
+            this.textBox_P.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_P.Location = new System.Drawing.Point(445, 41);
             this.textBox_P.Name = "textBox_P";
-            this.textBox_P.Size = new System.Drawing.Size(167, 20);
+            this.textBox_P.Size = new System.Drawing.Size(167, 26);
             this.textBox_P.TabIndex = 0;
+            this.textBox_P.Text = "3.1";
             // 
             // textBox_n
             // 
-            this.textBox_n.Location = new System.Drawing.Point(415, 139);
+            this.textBox_n.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_n.Location = new System.Drawing.Point(445, 72);
             this.textBox_n.Name = "textBox_n";
-            this.textBox_n.Size = new System.Drawing.Size(167, 20);
+            this.textBox_n.Size = new System.Drawing.Size(167, 26);
             this.textBox_n.TabIndex = 1;
+            this.textBox_n.Text = "115";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(163, 33);
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(165, 9);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(177, 17);
+            this.label3.Size = new System.Drawing.Size(257, 23);
             this.label3.TabIndex = 7;
-            this.label3.Text = "Вхідні данні (варіант №4):";
+            this.label3.Text = "Вхідні данні (варіант №17):";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(163, 142);
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(165, 73);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(246, 17);
+            this.label2.Size = new System.Drawing.Size(267, 19);
             this.label2.TabIndex = 6;
-            this.label2.Text = "Частота обертання на виході nв, хв.";
+            this.label2.Text = "Частота обертання на виході Nв, хв.-1";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(163, 111);
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(165, 42);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(196, 17);
+            this.label1.Size = new System.Drawing.Size(205, 19);
             this.label1.TabIndex = 5;
             this.label1.Text = "Потужність на виході Pв, кВт";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(616, 111);
+            this.button1.Location = new System.Drawing.Point(704, 42);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(109, 50);
+            this.button1.Size = new System.Drawing.Size(165, 56);
             this.button1.TabIndex = 8;
             this.button1.Text = "Розрахунок";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(415, 185);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 9;
-            // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(166, 211);
+            this.richTextBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(169, 436);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(730, 713);
+            this.richTextBox1.Size = new System.Drawing.Size(700, 805);
             this.richTextBox1.TabIndex = 10;
             this.richTextBox1.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(271, 104);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(527, 326);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1139, 963);
+            this.ClientSize = new System.Drawing.Size(1064, 1241);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -122,7 +134,9 @@
             this.Controls.Add(this.textBox_n);
             this.Controls.Add(this.textBox_P);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -136,7 +150,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
